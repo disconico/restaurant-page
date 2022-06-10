1df906b2e84bb4101b3142aa2a633136f5e4fe72
Nicolas Guerin <disconico@outlook.com>
2022-06-10 10:56:24 +0200

    Change webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     sourceMapFilename: "main.js.map",
-    // clean: true,
+    clean: true,
   },
   module: {
     rules: [
