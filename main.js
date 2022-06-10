8a33e1adfe89a1edf2d6327042a06a3cd8388241
Nicolas Guerin <disconico@outlook.com>
2022-06-10 10:04:12 +0200

    Set up main version

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hello friend_");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('hello friend_')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDRCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdoZWxsbyBmcmllbmRfJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
